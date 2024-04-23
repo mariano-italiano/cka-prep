45dcc6bb32149f31b05b9ccb1254c70700f08e29
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-04-23 22:45:26 +0200

    Create q9.sh

---
+++ q9.sh
@@ -0,0 +1,6 @@
+
+kubectl create ns enigma
+kubectl run backend-pod --image nginx -l app=backend -n enigma
+kubectl run db1-pod --image marcinkujawski/echo:1.1 -l app=db -n enigma
+kubectl run db2-pod --image marcinkujawski/echo:2.2 -l app=db -n enigma
+kubectl run vault-pod --image marcinkujawski/echo:3.3 -l app=vault -n enigma
